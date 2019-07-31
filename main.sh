2c69b680d8fd0b1903593f54063d1e4d68cf669b
test <test>
2019-07-31 19:59:40 +0200

    test

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
-sudo python3 "/home/pi/a/wordclock/main.py" \
---mqtt_server_ip "192.168.2.52" \
+sudo python3 "main.py" \
+--mqtt_server_ip "192.168.178.52" \
 --mqtt_server_port "1883" \
 --mqtt_topic_set_brightness "wordclock/brightness" \
 --mqtt_topic_set_mode "wordclock/mode" \
